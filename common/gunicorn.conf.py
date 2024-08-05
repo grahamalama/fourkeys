d43af4aec55a4de75f4f4fddbba3ad9e30a11372
Graham Beckley <gbeckley@mozilla.com>
2024-08-04 21:26:29 -0400

    Add debug mode to include access logs for gunicorn

--- common/gunicorn.conf.py
+++ common/gunicorn.conf.py
@@ -1,8 +1,10 @@
 import os
 
 PORT = int(os.getenv("PORT", "8000"))
+DEBUG = bool(os.getenv("DEBUG", "False"))
 
 bind = f":{PORT}"
 workers = 1 
 threads = 8 
 timeout = 0
+accesslog = "-" if DEBUG else None
